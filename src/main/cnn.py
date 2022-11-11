e7a594454069b7cb3060683cefdad11366612dd7
sud0 <gaspybaye@gmail.com>
2022-11-11 00:35:24 -0500

    fixed issues with saving to Saves directory

--- src/main/cnn.py
+++ src/main/cnn.py
@@ -4,48 +4,48 @@ from torch.nn import functional as F
 import os
 import plots
 
-
 ### user defined functions
 import Config
 from EndLayer import EndLayers
 import GPU
 
+
 class ModdedParallel(nn.DataParallel):
-    #From https://github.com/pytorch/pytorch/issues/16885#issuecomment-551779897
+    # From https://github.com/pytorch/pytorch/issues/16885#issuecomment-551779897
     def __getattr__(self, name):
         try:
             return super().__getattr__(name)
         except AttributeError:
             return getattr(self.module, name)
 
+
 class Conv1DClassifier(nn.Module):
     def __init__(self):
-            super().__init__()
-            self.layer1 = nn.Sequential(
-                nn.Conv1d(1, 32, 3),
-                nn.ReLU(),
-                nn.MaxPool1d(4),
-                nn.Dropout(0.5))
-            self.layer2 = nn.Sequential(
-                nn.Conv1d(32, 64, 3),
-                nn.ReLU(),
-                nn.MaxPool1d(2),
-                nn.Dropout(0.5))
-                
-            self.flatten = nn.Flatten()
-            self.dropout = nn.Dropout()
-            self.fc1 = nn.Linear(11904, 256)
-            self.fc2 = nn.Linear(256, 15)
-            n=3 #This is the DOO for COOL, I will need to make some way of easily editing it.
-            #self.COOL = nn.Linear(256, 15*n)
+        super().__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv1d(1, 32, 3),
+            nn.ReLU(),
+            nn.MaxPool1d(4),
+            nn.Dropout(0.5))
+        self.layer2 = nn.Sequential(
+            nn.Conv1d(32, 64, 3),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+            nn.Dropout(0.5))
 
-            self.end = EndLayers(15,type="Soft",cutoff=Config.parameters["threshold"][0])
-            self.batchnum = 0
-            self.device = GPU.get_default_device()
-            self.store = GPU.to_device(torch.tensor([]), self.device),GPU.to_device(torch.tensor([]), self.device)
+        self.flatten = nn.Flatten()
+        self.dropout = nn.Dropout()
+        self.fc1 = nn.Linear(11904, 256)
+        self.fc2 = nn.Linear(256, 15)
+        n = 3  # This is the DOO for COOL, I will need to make some way of easily editing it.
+        # self.COOL = nn.Linear(256, 15*n)
 
+        self.end = EndLayers(15, type="Soft", cutoff=Config.parameters["threshold"][0])
+        self.batchnum = 0
+        self.device = GPU.get_default_device()
+        self.store = GPU.to_device(torch.tensor([]), self.device), GPU.to_device(torch.tensor([]), self.device)
 
-        # Specify how the data passes in the neural network
+    # Specify how the data passes in the neural network
     def forward(self, x: torch.Tensor):
         # x = to_device(x, device)
         x = x.float()
@@ -55,168 +55,171 @@ class Conv1DClassifier(nn.Module):
         x = self.flatten(x)
         x = F.relu(self.fc1(x))
         x = self.dropout(x)
-        if self.end.type!="COOL":
+        if self.end.type != "COOL":
             x = self.fc2(x)
         else:
             x = self.COOL(x)
         # print("in forward", F.log_softmax(x, dim=1))
         return x
-        #return F.log_softmax(x, dim=1)
+        # return F.log_softmax(x, dim=1)
+
 
 class FullyConnected:
     def __init__(self):
         pass
+
+
 class AttackTrainingClassification(Conv1DClassifier):
-        def __init__(self):
-            super().__init__()
-            # self.model = model
-            # self.batch = batch
-            # self.to_device = to_device
-            # self.device = device
-            # self.Y_Pred = Y_Pred
-            # self.Y_test = Y_test
+    def __init__(self):
+        super().__init__()
+        # self.model = model
+        # self.batch = batch
+        # self.to_device = to_device
+        # self.device = device
+        # self.Y_Pred = Y_Pred
+        # self.Y_test = Y_test
 
-        def training_step(self,batch):
-            data, labels = batch
-            out = self(data)  # Generate predictions
-            if self.end.type == "COOL":
-                labels = self.end.COOL_Label_Mod(labels)
-                out = torch.split(out.unsqueeze(dim=1), 15, dim=2)
-                out = torch.cat(out, dim=1)
-            # out = DeviceDataLoader(out, device)
-            loss = F.cross_entropy(out, labels)  # Calculate loss
-            torch.cuda.empty_cache()
-            # print("loss from training step ... ", loss)
-            return loss
+    def training_step(self, batch):
+        data, labels = batch
+        out = self(data)  # Generate predictions
+        if self.end.type == "COOL":
+            labels = self.end.COOL_Label_Mod(labels)
+            out = torch.split(out.unsqueeze(dim=1), 15, dim=2)
+            out = torch.cat(out, dim=1)
+        # out = DeviceDataLoader(out, device)
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        torch.cuda.empty_cache()
+        # print("loss from training step ... ", loss)
+        return loss
 
-        def evaluate(self, validationset):
-            self.batchnum = 0
-            outputs = [self.validation_step(batch) for batch in validationset]  ### reverted bac
-            return self.validation_epoch_end(outputs)
+    def evaluate(self, validationset):
+        self.batchnum = 0
+        outputs = [self.validation_step(batch) for batch in validationset]  ### reverted bac
+        return self.validation_epoch_end(outputs)
 
-        def accuracy(self, outputs, labels):
-            preds = torch.argmax(outputs, dim=1)
-            # print("preds from accuracy", preds)
-            # print("labels from accuracy", labels)
-            # Y_Pred.append(preds.tolist()[:])
-            # Y_test.append(labels.tolist()[:])
-            # preds = torch.tensor(preds)
-            self.store = torch.cat((self.store[0],preds)),torch.cat((self.store[1],labels))
-            return torch.tensor(torch.sum(preds == labels).item() / len(preds))
-            # def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
+    def accuracy(self, outputs, labels):
+        preds = torch.argmax(outputs, dim=1)
+        # print("preds from accuracy", preds)
+        # print("labels from accuracy", labels)
+        # Y_Pred.append(preds.tolist()[:])
+        # Y_test.append(labels.tolist()[:])
+        # preds = torch.tensor(preds)
+        self.store = torch.cat((self.store[0], preds)), torch.cat((self.store[1], labels))
+        return torch.tensor(torch.sum(preds == labels).item() / len(preds))
+        # def fit(epochs, lr, model, train_loader, val_loader, opt_func=torch.optim.SGD):
 
-        def fit(self,epochs, lr, train_loader, val_loader, opt_func):
-            history = []
-            optimizer = opt_func(self.parameters(), lr)
-            #torch.cuda.empty_cache()
-            if epochs > 0:
-                for epoch in range(epochs):
-                    # Training Phase
-                    self.train()
-                    train_losses = []
-                    num = 0
-                    for batch in train_loader:
-                        # batch = to_device(batch,device)
-                        #batch = DeviceDataLoader(batch, device)
-                        loss = self.training_step(batch)
+    def fit(self, epochs, lr, train_loader, val_loader, opt_func):
+        history = []
+        optimizer = opt_func(self.parameters(), lr)
+        # torch.cuda.empty_cache()
+        if epochs > 0:
+            for epoch in range(epochs):
+                # Training Phase
+                self.train()
+                train_losses = []
+                num = 0
+                for batch in train_loader:
+                    # batch = to_device(batch,device)
+                    # batch = DeviceDataLoader(batch, device)
+                    loss = self.training_step(batch)
 
-                        plots.write_batch_to_file(loss, num, self.end.type, "train")
-                        train_losses.append(loss.detach())
-                        loss.backward()
-                        optimizer.step()
-                        optimizer.zero_grad()
-                        num += 1
+                    plots.write_batch_to_file(loss, num, self.end.type, "train")
+                    train_losses.append(loss.detach())
+                    loss.backward()
+                    optimizer.step()
+                    optimizer.zero_grad()
+                    num += 1
 
-                    # Validation phase
-                    self.savePoint(f"Saves", epoch, Config.helper_variables["phase"])
-                    result = self.evaluate(val_loader)
-                    result['train_loss'] = torch.stack(train_losses).mean().item()
-                    result["epoch"] = epoch
-                    self.epoch_end(epoch, result)
-                    print("result", result)
-
-                    history.append(result)
-            else:
                 # Validation phase
-                self.loadPoint("Saves")
+                self.savePoint(f"Saves", epoch, Config.helper_variables["phase"])
                 result = self.evaluate(val_loader)
-                result['train_loss'] = -1
-                self.epoch_end(0, result)
+                result['train_loss'] = torch.stack(train_losses).mean().item()
+                result["epoch"] = epoch
+                self.epoch_end(epoch, result)
                 print("result", result)
+
                 history.append(result)
-            return history
+        else:
+            # Validation phase
+            self.loadPoint("Saves")
+            result = self.evaluate(val_loader)
+            result['train_loss'] = -1
+            self.epoch_end(0, result)
+            print("result", result)
+            history.append(result)
+        return history
 
-        def validation_step(self,batch):
-            self.eval()
-            self.savePoint("test", phase=Config.helper_variables["phase"])
-            data, labels = batch
-            out = self(data)  # Generate predictions
-            out = self.end.endlayer(out,
-                                    labels)  # <----Here is where it is using Softmax TODO: make this be able to run all of the versions and save the outputs.
-            # out = self.end.endlayer(out, labels, type="Open")
-            # out = self.end.endlayer(out, labels, type="Energy")
+    def validation_step(self, batch):
+        self.eval()
+        self.savePoint("test", phase=Config.helper_variables["phase"])
+        data, labels = batch
+        out = self(data)  # Generate predictions
+        out = self.end.endlayer(out,
+                                labels)  # <----Here is where it is using Softmax TODO: make this be able to run all of the versions and save the outputs.
+        # out = self.end.endlayer(out, labels, type="Open")
+        # out = self.end.endlayer(out, labels, type="Energy")
 
-            # Y_pred = out
-            # Y_test = labels
-            # print("y-test from validation",Y_test)
-            # print("y-pred from validation", Y_pred)
-            unknowns = out[:, 15].mean()
-            out = GPU.to_device(out, self.device)
-            test = torch.argmax(out,dim=1)
-            loss = F.cross_entropy(out, labels)  # Calculate loss
-            plots.write_batch_to_file(loss, self.batchnum, self.end.type, "test")
-            self.batchnum += 1
-            acc = self.accuracy(out, labels)  # Calculate accuracy
-            print("validation accuracy: ", acc)
-            return {'val_loss': loss.detach(), 'val_acc': acc, "val_avgUnknown": unknowns}
+        # Y_pred = out
+        # Y_test = labels
+        # print("y-test from validation",Y_test)
+        # print("y-pred from validation", Y_pred)
+        unknowns = out[:, 15].mean()
+        out = GPU.to_device(out, self.device)
+        test = torch.argmax(out, dim=1)
+        loss = F.cross_entropy(out, labels)  # Calculate loss
+        plots.write_batch_to_file(loss, self.batchnum, self.end.type, "test")
+        self.batchnum += 1
+        acc = self.accuracy(out, labels)  # Calculate accuracy
+        print("validation accuracy: ", acc)
+        return {'val_loss': loss.detach(), 'val_acc': acc, "val_avgUnknown": unknowns}
 
-        def validation_epoch_end(self, outputs):
-            batch_losses = [x['val_loss'] for x in outputs]
-            epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
-            batch_accs = [x['val_acc'] for x in outputs]
-            epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
-            batch_unkn = self.end.Save_score
-            self.end.Save_score = []
-            epoch_unkn = torch.stack(batch_unkn).mean()  # Combine Unknowns
+    def validation_epoch_end(self, outputs):
+        batch_losses = [x['val_loss'] for x in outputs]
+        epoch_loss = torch.stack(batch_losses).mean()  # Combine losses
+        batch_accs = [x['val_acc'] for x in outputs]
+        epoch_acc = torch.stack(batch_accs).mean()  # Combine accuracies
+        batch_unkn = self.end.Save_score
+        self.end.Save_score = []
+        epoch_unkn = torch.stack(batch_unkn).mean()  # Combine Unknowns
 
-            return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item(), "val_avgUnknown": epoch_unkn.item()}
+        return {'val_loss': epoch_loss.item(), 'val_acc': epoch_acc.item(), "val_avgUnknown": epoch_unkn.item()}
 
-        def epoch_end(self, epoch, result):
-            print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(epoch,
-                                                                                             result['train_loss'],
-                                                                                             result['val_loss'],
-                                                                                             result['val_acc']))
+    def epoch_end(self, epoch, result):
+        print("Epoch [{}], train_loss: {:.4f}, val_loss: {:.4f}, val_acc: {:.4f}".format(epoch,
+                                                                                         result['train_loss'],
+                                                                                         result['val_loss'],
+                                                                                         result['val_acc']))
 
-        def savePoint(net:Conv1DClassifier, path: str, epoch=0, phase=None):
-            if not os.path.exists(path):
-                os.mkdir(path)
-           # torch.save(net.state_dict(), path + f"/Epoch{epoch:03d}.pth")
-            if phase is not None:
-                file = open("Saves/phase", "w")
-                file.write(str(phase))
-                file.close()
+    def savePoint(net: Conv1DClassifier, path: str, epoch=0, phase=None):
+        if not os.path.exists(path):
+            os.mkdir(path)
+        torch.save(net.state_dict(), path + f"/Epoch{epoch:03d}.pth")
+        if phase is not None:
+            file = open("Saves/phase", "w")
+            file.write(str(phase))
+            file.close()
 
-        def loadPoint(net:Conv1DClassifier, path: str):
-            if not os.path.exists(path):
-                os.mkdir(path)
-            i = 999
-            epochFound = 0
-            while i >= 0:
-                if os.path.exists(path + f"/Epoch{i:03d}.pth"):
-                    net.load_state_dict(torch.load(path + f"/Epoch{i:03d}.pth"))
-                    print(f"Loaded  model /Epoch{i:03d}.pth")
-                    epochFound = i
-                    i = -1
-                i = i - 1
-            if i != -2:
-                print("No model to load found.")
-            elif os.path.exists("Saves/phase"):
-                file = open("Saves/phase", "r")
-                phase = file.read()
-                file.close()
-                return int(phase), epochFound
-            return -1, -1
-                                                                                    
+    def loadPoint(net: Conv1DClassifier, path: str):
+        if not os.path.exists(path):
+            os.mkdir(path)
+        i = 999
+        epochFound = 0
+        while i >= 0:
+            if os.path.exists(path + f"/Epoch{i:03d}.pth"):
+                net.load_state_dict(torch.load(path + f"/Epoch{i:03d}.pth"))
+                print(f"Loaded  model /Epoch{i:03d}.pth")
+                epochFound = i
+                i = -1
+            i = i - 1
+        if i != -2:
+            print("No model to load found.")
+        elif os.path.exists("Saves/phase"):
+            file = open("Saves/phase", "r")
+            phase = file.read()
+            file.close()
+            return int(phase), epochFound
+        return -1, -1
+
 
 
 
--- src/main/plots.py
+++ src/main/plots.py
@@ -7,14 +7,13 @@ import Dataload
 from torch.utils.data import DataLoader
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import ConfusionMatrixDisplay
-from sklearn.metrics import confusion_matrix,accuracy_score
+from sklearn.metrics import confusion_matrix, accuracy_score
 from sklearn.metrics import (precision_score, recall_score,
-                             f1_score,confusion_matrix,average_precision_score)
+                             f1_score, confusion_matrix, average_precision_score)
 import itertools
 import numpy as np
 import itertools
 import pandas as pd
-
 
 
 # epoch = 20 history_final [{'val_loss': 3.6822474002838135, 'val_acc': 0.29999998211860657, 'train_loss': 11.54269027709961}, {'val_loss': 1.771144151687622, 'val_acc': 0.459090918302536, 'train_loss': 2.622060537338257}, {'val_loss': 1.49733304977417, 'val_acc': 0.47272729873657227, 'train_loss': 1.668800711631775}, {'val_loss': 1.4760304689407349, 'val_acc': 0.5136363506317139, 'train_loss': 1.4828541278839111}, {'val_loss': 1.4107227325439453, 'val_acc': 0.5590909719467163, 'train_loss': 1.355711579322815}, {'val_loss': 1.3187934160232544, 'val_acc': 0.5363636612892151, 'train_loss': 1.436032772064209}, {'val_loss': 1.171736717224121, 'val_acc': 0.550000011920929, 'train_loss': 1.3234833478927612}, {'val_loss': 1.09578275680542, 'val_acc': 0.5954546332359314, 'train_loss': 1.1878279447555542}, {'val_loss': 0.9965360164642334, 'val_acc': 0.663636326789856, 'train_loss': 1.134917140007019}, {'val_loss': 1.0754733085632324, 'val_acc': 0.6136363744735718, 'train_loss': 1.0401008129119873}, {'val_loss': 1.015233039855957, 'val_acc': 0.668181836605072, 'train_loss': 1.0170620679855347}, {'val_loss': 0.9538360834121704, 'val_acc': 0.6318181753158569, 'train_loss': 0.9501714706420898}, {'val_loss': 0.9685512185096741, 'val_acc': 0.6590909361839294, 'train_loss': 0.9937074780464172}, {'val_loss': 0.9329931139945984, 'val_acc': 0.7090909481048584, 'train_loss': 0.9353311061859131}, {'val_loss': 0.8152506351470947, 'val_acc': 0.7272727489471436, 'train_loss': 1.0522938966751099}, {'val_loss': 0.7926329970359802, 'val_acc': 0.7409090399742126, 'train_loss': 0.9276139736175537}, {'val_loss': 0.790667712688446, 'val_acc': 0.7000000476837158, 'train_loss': 0.8848629593849182}, {'val_loss': 0.7107415795326233, 'val_acc': 0.7363636493682861, 'train_loss': 0.860095202922821}, {'val_loss': 0.7106726765632629, 'val_acc': 0.75, 'train_loss': 0.810321569442749}, {'val_loss': 0.7572275400161743, 'val_acc': 0.7545454502105713, 'train_loss': 0.699836015701294}]
@@ -24,14 +23,16 @@ import pandas as pd
 # Y_test = [[12, 11, 0, 7, 1, 4, 8, 6, 5, 5], [4, 0, 5, 10, 7, 6, 9, 0, 11, 0], [0, 12, 0, 11, 6, 5, 0, 7, 11, 8], [6, 4, 0, 9, 5, 7, 1, 10, 4, 5]]
 # Y_pred = [[9, 0, 0, 8, 7, 5, 9, 0, 0, 6], [6, 0, 5, 0, 0, 0, 9, 0, 0, 0], [7, 7, 0, 12, 5, 4, 7, 7, 7, 9], [7, 0, 7, 9, 8, 7, 4, 7, 5, 7]]
 
-def convert_to_1d(y_test,y_pred):
+def convert_to_1d(y_test, y_pred):
     y_test_final = []
     y_pred_final = []
     for i in range(len(y_test)):
         for j in range(len(y_pred[i])):
             y_test_final.append(y_test[i][j])
             y_pred_final.append(y_pred[i][j])
-    return y_test_final,y_pred_final
+    return y_test_final, y_pred_final
+
+
 # y_test, y_pred = convert_to_1d(Y_test,Y_pred)
 
 def plot_losses(history):
@@ -42,7 +43,7 @@ def plot_losses(history):
     plt.ylabel('loss')
     plt.legend(['losses'])
     plt.title('Loss vs. No. of epochs')
-    plt.savefig("/Saves/plot_losses.png", dpi=600)
+    plt.savefig("Saves/plot_losses.png", dpi=600)
     plt.show()
 
 
@@ -54,8 +55,8 @@ def plot_accuracies(history):
     plt.ylabel('accuracy')
     plt.legend(['accuracy'])
     plt.title('Accuracy vs. No. of epochs')
-    plt.ylim([0,1])
-    plt.savefig("/Saves/plot_accuracies.png", dpi=600)
+    plt.ylim([0, 1])
+    plt.savefig("Saves/plot_accuracies.png", dpi=600)
     plt.show()
 
 
@@ -68,20 +69,16 @@ def plot_all_losses(history):
     plt.ylabel('loss')
     plt.legend(['Training_loss', 'val_loss'])
     plt.title('train & val Loss vs. No. of epochs')
-    plt.savefig("/Saves/plot_all_losses.png", dpi=600)
+    plt.savefig("Saves/plot_all_losses.png", dpi=600)
     plt.show()
+
 
 # plot_all_losses(history_final)
 # plot_losses(history_final)
 # plot_accuracies(history_final)
 
-def confusionMatrix(y_test,y_pred):
+def confusionMatrix(y_test, y_pred):
     return confusion_matrix(y_test, y_pred)
-
-
-
-
-
 
 
 # def plot_confusion_matrix(y_test,y_pred):
@@ -102,6 +99,7 @@ def confusionMatrix(y_test,y_pred):
 import Dataload
 import math
 
+
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
@@ -119,17 +117,16 @@ def plot_confusion_matrix(cm, classes,
 
     if normalize:
         cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
-        #cm = "{:.2f}".format(float)
+        # cm = "{:.2f}".format(float)
         print("Normalized confusion matrix")
     else:
         print('Confusion matrix, without normalization')
 
-    print("cm",cm.shape)
+    print("cm", cm.shape)
 
     thresh = cm.max() / 2.
     for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-
-        #print("cm ij ",str(round(cm[i, j], 2)))
+        # print("cm ij ",str(round(cm[i, j], 2)))
         plt.text(j, i, str(round(cm[i, j], 2)),
                  horizontalalignment="center",
                  color="white" if cm[i, j] > thresh else "black")
@@ -137,7 +134,8 @@ def plot_confusion_matrix(cm, classes,
     plt.tight_layout()
     plt.ylabel('True label')
     plt.xlabel('Predicted label')
-    plt.savefig("/Saves/confusion_matrix.png", dpi=600)
+    plt.savefig("Saves/confusion_matrix.png", dpi=600)
+
 
 # Compute confusion matrix
 # cnf_matrix = confusion_matrix(y_test, y_pred)
@@ -153,66 +151,69 @@ def plot_confusion_matrix(cm, classes,
 #                       title='Normalized confusion matrix')
 # plt.show()
 
-def write_hist_to_file(lst,num_epochs,type=""):
-    
+def write_hist_to_file(lst, num_epochs, type=""):
     for l in lst:
         l["type"] = type
     if os.path.exists("Saves/history.csv"):
-        hist = pd.read_csv("Saves/history.csv",index_col=0)
-        hist = pd.concat((hist,pd.DataFrame.from_dict(lst)))
+        hist = pd.read_csv("Saves/history.csv", index_col=0)
+        hist = pd.concat((hist, pd.DataFrame.from_dict(lst)))
     else:
         hist = pd.DataFrame.from_dict(lst)
     hist.to_csv("Saves/history.csv")
     with open(f'Saves/history{type}.txt', 'a') as fp:
-        #fp.write(f"history for {num_epochs} \n")
+        # fp.write(f"history for {num_epochs} \n")
         fp.write("\n")
         for item in lst:
             # write each item on a new line
-            fp.write(f"num_epochs {num_epochs} "+str(item)+"\n")
+            fp.write(f"num_epochs {num_epochs} " + str(item) + "\n")
         print('Writing history Done')
-def write_scores_to_file(lst,num_epochs,type=""):
+
+
+def write_scores_to_file(lst, num_epochs, type=""):
     thisRun = pd.DataFrame.from_dict(lst)
     thisRun["type"] = type
     if os.path.exists("Saves/scores.csv"):
-        hist = pd.read_csv("Saves/scores.csv",index_col=0)
+        hist = pd.read_csv("Saves/scores.csv", index_col=0)
         hist.loc[len(hist)] = thisRun.iloc[0]
     else:
         hist = thisRun
-    
+
     hist.to_csv("Saves/scores.csv")
     with open(f'Saves/scores{type}.txt', 'a') as fp:
         fp.write("\n")
         for item in lst:
             # write each item on a new line
-            fp.write(f"num_epochs {num_epochs} "+str(item).format(num_epochs)+"\n")
+            fp.write(f"num_epochs {num_epochs} " + str(item).format(num_epochs) + "\n")
         print('Writing scores Done')
 
-def write_batch_to_file(loss, num, modeltype="",batchtype=""):
-    thisRun = pd.DataFrame([[loss.item(),num,modeltype,batchtype]],columns=["Loss","Batch Number","Model Type","Batch Type"])
+
+def write_batch_to_file(loss, num, modeltype="", batchtype=""):
+    thisRun = pd.DataFrame([[loss.item(), num, modeltype, batchtype]],
+                           columns=["Loss", "Batch Number", "Model Type", "Batch Type"])
     # thisRun["Loss"] = loss.detach()
     # thisRun["Batch Number"] = num
     # thisRun["Model Type"] = modeltype
     # thisRun["Batch Type"] = batchtype
     if os.path.exists("Saves/batch.csv"):
-        hist = pd.read_csv("Saves/batch.csv",index_col=0)
+        hist = pd.read_csv("Saves/batch.csv", index_col=0)
         hist.loc[len(hist)] = thisRun.iloc[0]
     else:
         hist = thisRun
-    
+
     hist.to_csv("Saves/batch.csv")
 
 
-def store_values(history:list, Y_predict:list, Y_test:list, num_epochs:int, end_type:str):
-    y_test, y_pred = convert_to_1d(Y_test,Y_predict)
-    recall = recall_score(y_test,y_pred,average='weighted',zero_division=0)
-    precision = precision_score(y_test,y_pred,average='weighted',zero_division=0)
+def store_values(history: list, Y_predict: list, Y_test: list, num_epochs: int, end_type: str):
+    y_test, y_pred = convert_to_1d(Y_test, Y_predict)
+    recall = recall_score(y_test, y_pred, average='weighted', zero_division=0)
+    precision = precision_score(y_test, y_pred, average='weighted', zero_division=0)
     f1 = 2 * (precision * recall) / (precision + recall)
     # auprc = average_precision_score(y_test, y_pred, average='samples')
-    score_list = [recall,precision,f1]
-    write_hist_to_file(history,num_epochs,end_type)
-    write_scores_to_file(score_list,num_epochs,end_type)  
+    score_list = [recall, precision, f1]
+    write_hist_to_file(history, num_epochs, end_type)
+    write_scores_to_file(score_list, num_epochs, end_type)
 
-#write_hist_to_file(history_final,num_epochs)
+# write_hist_to_file(history_final,num_epochs)
 # 0
 # 0,362108
 # 1,115007
@@ -229,7 +230,6 @@ def store_values(history:list, Y_predict:list, Y_test:list, num_epochs:int, end_
 # 12,11754
 # 13,3341
 # 14,12
-
 
 
 # 0
