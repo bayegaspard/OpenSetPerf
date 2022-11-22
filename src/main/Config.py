fbdcc8dbdd1bf5057989af65dd3c3a9223d695c4
sud0 <bgaspard@umassd.edu>
2022-11-22 09:51:24 -0500

    Updated config with working parameters
    Updated config with working parameters

--- src/main/Config.py
+++ src/main/Config.py
@@ -4,7 +4,7 @@ opt_func = {"Adam":torch.optim.Adam,"SGD":torch.optim.SGD}
 helper_variables = {
     "phase" : -1,
     "startphase" : 0,
-    "unknowns_clss": {"unknowns":[2, 3, 13,1,10,12,14]},
+    "unknowns_clss": {"unknowns":[2,3]},
     "e": 0
 }
 parameters = {
@@ -12,9 +12,9 @@ parameters = {
     "num_workers":[3, "Number of threads working on building batches"],
     "attemptLoad":[0, "0: do not use saves\n1:use saves"],
     "testlength":[1/4, "[0,1) percentage of training to test with"],
-    "num_epochs":[5,"Number of times it trains on the whole trainset"],
-    "learningRate":[0.01, "a modifier for training"],
-    "threshold":[0.25,"When to declare something to be unknown"],
+    "num_epochs":[10,"Number of times it trains on the whole trainset"],
+    "learningRate":[0.001, "a modifier for training"],
+    "threshold":[5,"When to declare something to be unknown"],
     "optimizer":opt_func["Adam"],
     "Unknowns":"refer to unknowns.CSV",
     "CLASSES":[15,"Number of classes, do not change"],
