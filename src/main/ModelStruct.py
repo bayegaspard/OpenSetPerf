18e4f4cfbc7d8932779332d2e086526fa76f6064
ambroggi <97543940+ambroggi@users.noreply.github.com>
2023-04-30 22:41:50 -0400

    Update ModelStruct.py

--- src/main/ModelStruct.py
+++ src/main/ModelStruct.py
@@ -267,7 +267,7 @@ class AttackTrainingClassification(nn.Module):
         out = self(data)  # Generate predictions
         #zeross = GPU.to_device(torch.zeros(len(out),1),device)
         out = self.end.endlayer(out,
-                                labels)  # <----Here is where it is using Softmax TODO: make this be able to run all of the versions and save the outputs.
+                                labels).to(labels.device)  # <----Here is where it is using Softmax TODO: make this be able to run all of the versions and save the outputs.
         #loss = F.cross_entropy(torch.cat((out,zeross),dim=1), labels)  # Calculate loss
         loss = F.cross_entropy(out, labels)
         # out = self.end.endlayer(out, labels, type="Open")
