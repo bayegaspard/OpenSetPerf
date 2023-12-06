afbdf6dd7ab47a8893a216611cab0a143ea944bb
ambroggi <abroggi@umassd.edu>
2023-12-06 15:05:32 -0500

    Update EndLayer.py

--- src/main/EndLayer.py
+++ src/main/EndLayer.py
@@ -80,7 +80,7 @@ class EndLayers(nn.Module):
             output_m_soft = self.typesOfMod.get("Soft",self.typesOfMod["none"])(self,output_true)
             output_c_soft = self.typesOfUnknown["Soft"](self,output_m_soft)
             var_mask = ~self.varmax_mask(output_true)
-            thresh_mask = output_true.max(dim=1)[0].less(0.5)
+            thresh_mask = torch.softmax(output_true,dim=1).max(dim=1)[0].greater(0.5)
             output_complete[var_mask&thresh_mask] = output_c_soft[var_mask&thresh_mask]
 
 
