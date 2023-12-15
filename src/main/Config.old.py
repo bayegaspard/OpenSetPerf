6e487876fcfaea20702b9584d37cd3b73890d03f
sud0 <gaspybaye@gmail.com>
2023-12-15 17:26:39 -0500

    updated the config file
    Improved the config to have the updated architecture

--- src/main/Config.old.py
+++ src/main/Config.old.py
@@ -54,41 +54,42 @@ parameters = {
     #These parameters are orginized like this:
     #"ParamName":[Value, "Description", [possible values]]
     #for a parameter called "ParamName" with a value of Value
-    "batch_size":[100000, "Number of items per batch"],
+    "batch_size":[10, "Number of items per batch"],
     "num_workers":[14, "Number of threads working on building batches"],
     "attemptLoadModel":[0, "0: do not use saves for the model\n1:use saves for the model"],
     "attemptLoadData":[0, "0: do not use saves for the dataset\n1:use saves for the dataset"],
     "testlength":[1/4, "[0,1) percentage of training to test with"],
     "Mix unknowns and validation": [1,"0 or 1, 0 means that the test set is purely unknowns and 1 means that the testset is the validation set plus unknowns (for testing)"],
-    "MaxPerClass": [20, "Maximum number of samples per class\n if Dataloader_Variation is Cluster and this value is a float it interprets it as the maximum percentage of the class instead."],
-    "num_epochs":[150,"Number of times it trains on the whole trainset"],
+    "MaxPerClass": [100, "Maximum number of samples per class\n if Dataloader_Variation is Cluster and this value is a float it interprets it as the maximum percentage of the class instead."],
+    "num_epochs":[10,"Number of times it trains on the whole trainset"],
     "learningRate":[0.01, "a modifier for training"],
-    "threshold":[1,"When to declare something to be unknown"],
+    "threshold":[0.25,"When to declare something to be unknown"],
     "model":["Convolutional","Model type",["Fully_Connected","Convolutional"]],
-    "OOD Type":["Soft","type of out of distribution detection", ["Soft","Open","Energy","COOL","DOC","iiMod", "Var", "SoftThresh"]],
+    "OOD Type":["Soft","type of out of distribution detection", ["Soft","Open","Energy","COOL","DOC","iiMod", "Var"]],
     "Dataloader_Variation":["Standard","Defines the style of Dataloader used. This affects sampling from the dataset", ["Standard","Cluster","Flows"]],
     "Dropout":[0.05,"percent of nodes that are skipped per run, larger numbers for more complex models [0,1)"],
     "optimizer":opt_func["Adam"],
     "Unknowns":["UNUSED"],
-    "Unknowns_clss": [[10, 11, 12, 13, 14],"Class indexes used as unknowns."],
+    "Unknowns_clss": [[7,8,9],"Class indexes used as unknowns."],
     "CLASSES":[15,"Number of classes, do not change"],
     "Temperature":[1,"Energy OOD scaling parameter"],
     "Degree of Overcompleteness": [3,"Parameter for Fitted Learning"],
     "Number of Layers": [2,"Number of layers to add to the base model"],
     "Nodes": [512,"The number of nodes per added layer"],
     "Activation": ["Leaky","The type of activation function to use",["ReLU", "Tanh", "Sigmoid","Leaky"]],
-    "LOOP": [4,"This is a parameter that determines if we want to loop over the algorithms.\n "\
-    "0: no loop, \n"\
-    "1:loop through variations of algorithms,thresholds,learning rates, groups and numbers of epochs, \n"\
-    "2: Loop while adding more unknowns into the training data (making them knowns) without resetting the model, \n"\
-    "3: Loop through different data distributions without training the model.\n"\
-    "4: Loop through predefined hyperparameters found in datasets/hyperparamList.csv"],
+    "LOOP": [0, "This is a parameter that determines if we want to loop over the algorithms.\n "\
+                "0: no loop, \n"\
+                "1:loop through variations of algorithms,thresholds,learning rates, groups and numbers of epochs, \n"\
+                "2: Loop while adding more unknowns into the training data (making them knowns) without resetting the model, \n"\
+                "3: Loop through different data distributions without training the model.\n"\
+                "4: Loop through predefined hyperparameters found in datasets/hyperparamList.csv"],
     "Dataset": ["Payload_data_CICIDS2017", "This is what dataset we are using,", ["Payload_data_CICIDS2017","Payload_data_UNSW"]],
     "SchedulerStepSize": [10, "This is how often the scheduler takes a step, 3 means every third epoch"],
     "SchedulerStep": [0.8,"This is how big a step the scheduler takes, leave 0 for no step"],
     "ApplyPrelimSoft": [0, "This says to use a preliminary softmax and only use unknown detection on things that fail the softmax unknown detection"],
     "ItemLogitData": [0, "1: use item logit data and store it in 'Saves/item.csv', 0: disabled"],
-    "SaveBatchData": [0, "1: Save some data from each batch in 'Saves/BatchSaves.csv', 0: disabled"],
+    "SaveBatchData": [1, "1: Save some data from each batch in 'Saves/BatchSaves.csv', 0: disabled"],
+    #"Var_filtering_threshold": [-1, "If not -1, the model will first apply a varmax layer to the endlayer to know if the 'OOD Type' algorithm should be applied. This will use the number given as the threshold."],
     "Var_filtering_threshold": [[1.2,12.4], "If not -1, the model will first apply a varmax layer to the endlayer to know if the 'OOD Type' algorithm should be applied. This will use the number given as the threshold."],
     "Experimental_bitConvolution": [0, "Convolutional network contains an experimental 2 bit convolution over the bits"]
 }
